c3fb86f1c10acf6b1476e66ffe02b916a504d571
Marcos Amorim <markstm10@gmail.com>
2021-09-03 02:14:45 -0300

    🛠 criado constante para armazenar a quantidade de um produto no carrinho utilizando reduce. Modificado function para carregar produtos no state da home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -25,22 +25,30 @@ const Home = (): JSX.Element => {
   const [products, setProducts] = useState<ProductFormatted[]>([]);
   const { addProduct, cart } = useCart();
 
-  //const cartItemsAmount = cart.reduce((sumAmount, product) => {
-  //TODO;
-  // }, {} as CartItemsAmount);
+  const cartItemsAmount = cart.reduce((sumAmount, product) => {
+    const newSumAmount = { ...sumAmount };
+    newSumAmount[product.id] = product.amount;
+
+    return newSumAmount;
+  }, {} as CartItemsAmount);
   console.log(products);
+
   useEffect(() => {
     async function loadProducts() {
-      api.get("products").then((response) => {
-        setProducts(response.data);
-      });
+      const response = await api.get<Product[]>("products");
+      const data = response.data.map((product) => ({
+        ...product,
+        priceFormatted: formatPrice(product.price),
+      }));
+
+      setProducts(data);
     }
 
     loadProducts();
   }, []);
 
-  function handleAddProduct(productId: number) {
-    addProduct(productId);
+  function handleAddProduct(id: number) {
+    addProduct(id);
   }
 
   return (
@@ -50,7 +58,7 @@ const Home = (): JSX.Element => {
           <li key={product.id}>
             <img src={product.image} alt={product.title} />
             <strong>{product.title}</strong>
-            <span>{`${formatPrice(product.price)}`}</span>
+            <span>{product.priceFormatted}</span>
             <button
               type="button"
               data-testid="add-product-button"
@@ -58,7 +66,7 @@ const Home = (): JSX.Element => {
             >
               <div data-testid="cart-product-quantity">
                 <MdAddShoppingCart size={16} color="#FFF" />
-                {/* {cartItemsAmount[product.id] || 0} */} 2
+                {cartItemsAmount[product.id] || 0}
               </div>
 
               <span>ADICIONAR AO CARRINHO</span>
